28ceaa150631dc5e122c43e34e28ad26cc279587
Arturo Monroy Orea <arturomonroy1720@gmail.com>
2023-12-26 21:23:45 -0600

    subiendo archivos

---
+++ src/journal/pages/JournalPage.jsx
@@ -0,0 +1,11 @@
+import { Typography } from "@mui/material";
+
+const JournalPage = () => {
+  return (
+    <>
+      <Typography component='h1'>JournalPage</Typography>
+    </>
+  )
+}
+
+export default JournalPage
